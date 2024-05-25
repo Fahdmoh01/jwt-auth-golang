835e114c7700a8cfe724a0ca350e685fdb3f000a
Fahd Mohammed <fahdmoh.1@gmail.com>
2024-05-25 13:24:58 +0100

    added .env to .gitignore

---
+++ controllers/usersControllers.go
@@ -0,0 +1,14 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+
+func Signup(c *gin.Context){
+	// Get the email/pass off request body
+
+	// Hash the password
+
+	//Create the user
+
+	//Respond 
+}
